1f6f512b3c7143bd59b5ff07cd1482a48657cb70
Emanuel Henrique <emanuelvieiraDev@gmail.com>
2022-05-16 11:36:41 -0300

    project finish

---
+++ src/components/Continent.tsx
@@ -0,0 +1,71 @@
+import { Flex, Stack, Heading, Text, Image, Box } from "@chakra-ui/react";
+import { ReactElement } from "react";
+import { CitiesCard } from "./CitiesCard";
+
+interface PageContinentProps {
+    mainImage: string; 
+    qtdCountry: number;
+    qtdLanguages: number;
+    qtdCities: number;
+    description: string;
+    nameContinent: string;
+    children: ReactElement;
+}
+
+
+export function Continent ({ mainImage, qtdCountry, qtdLanguages, qtdCities, description, nameContinent, children } : PageContinentProps) {
+
+    return (
+        <>
+            <Box>
+            
+                <Image objectFit="cover" width="100%" maxHeight={["150", "500"]} src={mainImage} alt="Imagem de um continente" />
+                
+                <Heading 
+                    position="absolute" 
+                    top="450" 
+                    color="white"
+                    ml="20"
+                    >{nameContinent}</Heading>
+            
+            </Box>
+            
+            <Flex 
+                m={8} 
+                direction={["column", "row"]} 
+                width="90%" 
+                gap="10" 
+                alignItems="center"
+                padding="20"
+            >
+
+                <Text display="block" width="100%">
+                    {description}
+                </Text>
+
+                
+
+                <Flex w="100%"  justify="center">
+
+                    <Stack spacing="3" direction="row">
+                        <Heading fontSize={25}>  <Text color="#FFBA08">{qtdCountry}</Text> Países</Heading>
+                   
+                        <Heading fontSize={25}><Text color="#FFBA08">{qtdLanguages}</Text> Línguas</Heading>
+
+                        <Heading fontSize={25}><Text color="#FFBA08">{qtdCities}</Text> Cidades</Heading>         
+                    </Stack>  
+                        
+                </Flex>
+
+
+            
+            </Flex>
+
+            {children} 
+        
+        </>
+
+    )
+
+
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import { Box } from '@chakra-ui/react'
+import  {Image}  from "@chakra-ui/react";
+
+export function Header(){
+
+    return (
+        <Box bg="color.headings_and_text" w="100%" h="12"  p="3" align="center" >
+                <Image
+                    src="./images/logo.svg"
+                    height={30}
+                    alt="sdasdad"
+                /> 
+        </Box>
+    )
+
+}
